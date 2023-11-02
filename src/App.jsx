dd54f24f5e1c1aed6ae79c64cd0a3b48cf583c35
Hamza Ahmed <hamza.anwar@outlook.com>
2023-11-02 14:55:14 +0000

    done core

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,60 @@
-import Header from './components/header'
-import initialEmails from './data/emails'
-
-import './styles/App.css'
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
+import { useState } from "react";
+import "./styles/App.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails);
+  console.log(emails);
 
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
-      <nav className="left-menu">
-        <ul className="inbox-list">
+      <nav className='left-menu'>
+        <ul className='inbox-list'>
           <li
-            className="item active"
+            className='item active'
             // onClick={() => {}}
           >
-            <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className='label'>Inbox</span>
+            <span className='count'>?</span>
           </li>
           <li
-            className="item"
+            className='item'
             // onClick={() => {}}
           >
-            <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className='label'>Starred</span>
+            <span className='count'>?</span>
           </li>
 
-          <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+          <li className='item toggle'>
+            <label for='hide-read'>Hide read</label>
             <input
-              id="hide-read"
-              type="checkbox"
+              id='hide-read'
+              type='checkbox'
               checked={false}
               // onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className='emails'>
+        {emails.map((email) => (
+          <li className='email' key={email.id}>
+            <div className='select'>
+              <input className='select-checkbox' type='checkbox' />
+            </div>
+            <div className='star'>
+              <input className='star-checkbox' type='checkbox' />
+            </div>
+            <div className='sender'>{email.sender}</div>
+            <div className='title'>{email.title}</div>
+          </li>
+        ))}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
